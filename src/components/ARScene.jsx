0d29d13fe35539194548e2c80d8c18931ae04530
EnFloky <paudelolmo@gmail.com>
2025-03-10 13:07:04 +0100

    Correcció final: evitada càrrega doble de Three.js i A-Frame

--- src/components/ARScene.jsx
+++ src/components/ARScene.jsx
@@ -1,8 +1,9 @@
+import React, { useState, useEffect } from "react";
 import 'aframe';
 import '@ar-js-org/ar.js';
-import { useState, useEffect } from 'react';
+import PeakFinderPanel from "./components/PeakFinderPanel";
 
-const ARScene = () => {
+function App() {
   const [userLocation, setUserLocation] = useState(null);
   const [userHeading, setUserHeading] = useState(null);
   const [mountains, setMountains] = useState([
@@ -36,49 +37,57 @@ const ARScene = () => {
   }, []);
 
   return (
-    <a-scene embedded arjs="sourceType: webcam; debugUIEnabled: false;" vr-mode-ui="enabled: false">
-      {/* Càmera AR */}
-      <a-camera gps-camera rotation-reader></a-camera>
+    <div className="App">
+      <h1>100CimsAR</h1>
 
-      {userLocation && userHeading &&
-        mountains.map((mountain, index) => {
-          // Calcular el rumb cap a la muntanya
-          const bearing = calculateBearing(userLocation, mountain);
+      {/* Panell 2D de PeakFinder (o un mapa de muntanyes) */}
+      <PeakFinderPanel />
 
-          return (
-            <a-entity
-              key={index}
-              gps-entity-place={`latitude: ${mountain.latitude}; longitude: ${mountain.longitude}`}
-              position={`0 ${mountain.altitude - userLocation.altitude + 50} 0`}
-              rotation={`0 ${bearing - userHeading} 0`} // Ajustar rotació segons l'orientació
-            >
-              {/* Afegir un fons amb el nom de la muntanya */}
-              <a-plane
-                position="0 0 0"
-                width="5"
-                height="1.5"
-                color="#32a852"
-                opacity="0.8"
-                look-at="[gps-camera]"
+      {/* Escena AR amb la càmera */}
+      <a-scene embedded arjs="sourceType: webcam; debugUIEnabled: false;" vr-mode-ui="enabled: false">
+        {/* Càmera per a AR */}
+        <a-camera gps-camera rotation-reader></a-camera>
+
+        {userLocation && userHeading &&
+          mountains.map((mountain, index) => {
+            // Calcular el rumb cap a la muntanya
+            const bearing = calculateBearing(userLocation, mountain);
+
+            return (
+              <a-entity
+                key={index}
+                gps-entity-place={`latitude: ${mountain.latitude}; longitude: ${mountain.longitude}`}
+                position={`0 ${mountain.altitude - userLocation.altitude + 50} 0`}
+                rotation={`0 ${bearing - userHeading} 0`} // Ajustar rotació segons l'orientació
               >
-                <a-text
-                  value={mountain.name + " - " + mountain.altitude + "m"}
+                {/* Afegir un fons amb el nom de la muntanya */}
+                <a-plane
+                  position="0 0 0"
+                  width="5"
+                  height="1.5"
+                  color="#32a852"
+                  opacity="0.8"
                   look-at="[gps-camera]"
-                  color="white"
-                  align="center"
-                  font="mozillavr"
-                  width="4"
-                  scale="2 2 2"
-                ></a-text>
-              </a-plane>
-            </a-entity>
-          );
-        })}
-    </a-scene>
+                >
+                  <a-text
+                    value={mountain.name + " - " + mountain.altitude + "m"}
+                    look-at="[gps-camera]"
+                    color="white"
+                    align="center"
+                    font="mozillavr"
+                    width="4"
+                    scale="2 2 2"
+                  ></a-text>
+                </a-plane>
+              </a-entity>
+            );
+          })}
+      </a-scene>
+    </div>
   );
-};
+}
 
-export default ARScene;
+export default App;
 
 // Funció per calcular el rumb entre la ubicació de l'usuari i la muntanya
 const calculateBearing = (userLocation, mountain) => {
